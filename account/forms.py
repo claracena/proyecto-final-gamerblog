34692d405036e3a739b10ebc43448a0ed7e1f646
claracena <claracena@users.noreply.github.com>
2022-06-18 20:19:53 -0300

    Account funcional... y hasta ahi nomas

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from account.models import Account
 
 class RegistrationForm(UserCreationForm):
@@ -41,3 +41,19 @@ class AccountAuthenticationForm(forms.ModelForm):
                 password = self.cleaned_data['password']
                 if not authenticate(email=email, password=password):
                     raise forms.ValidationError('Invalid Login')
+
+class EditProfileForm(UserChangeForm):
+
+    class Meta:
+        model = Account
+        fields = ('profile_image', 'fname', 'lname', 'bio')
+
+    # def save(self, commit=True):
+    #     account = super(EditProfileForm, self).save(commit=False)
+    #     account.fname = self.cleaned_data['fname']
+    #     account.lname = self.cleaned_data['lname']
+    #     account.bio = self.cleaned_data['bio']
+    #     account.profile_image = self.cleaned_data['profile_image']
+    #     if commit:
+    #         account.save()
+    #     return account
