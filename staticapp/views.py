21c574f9c7ecf7634c3cf372158342f3cf26e3c1
claracena <claracena@users.noreply.github.com>
2022-06-19 13:26:40 -0300

    random bgs

--- staticapp/views.py
+++ staticapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from random import randrange
+
+def background():
+    number = randrange(1, 10)
+    return f'{number:02d}'
 
 # Create your views here.
 def home(request):
-    return render(request, 'staticapp/inicio.html')
+    context = {'bg': f'bg-{background()}.jpg'}
+    return render(request, 'staticapp/inicio.html', context)
