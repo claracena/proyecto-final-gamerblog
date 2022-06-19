25328de2552721b290e61dbe207fc4e10dec121d
claracena <claracena@users.noreply.github.com>
2022-06-19 16:43:11 -0300

    backgrounds dinamicos ok

--- staticapp/views.py
+++ staticapp/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
-from random import randrange
-
-def background():
-    number = randrange(1, 10)
-    return f'{number:02d}'
 
 # Create your views here.
 def home(request):
-    context = {'bg': f'bg-{background()}.jpg'}
-    return render(request, 'staticapp/inicio.html', context)
+    return render(request, 'staticapp/inicio.html')
