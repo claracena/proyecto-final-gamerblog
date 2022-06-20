c1e3b49760ff92882830b8f7bdd1e74c5980ed4e
joseorozco84 <joseorozco84@gmail.com>
2022-06-20 16:00:22 -0300

    blog updates

--- blog/models.py
+++ blog/models.py
@@ -4,16 +4,13 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=255)
-    pub_date = models.DateTimeField()
+    pub_date = models.DateTimeField(auto_now_add=True)
     body = models.TextField()
     image = models.ImageField(blank=True, null=True)
     comments = models.IntegerField(default=0)
     category = models.CharField(max_length=255)
-    author = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    # author = models.OneToOneField('account.Account', on_delete=models.CASCADE)
 
 
     def __str__(self):
         return self.title
-
-    def summary(self):
-        return self.body[100]
--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,6 @@ from blog.models import Blog
 
 # Create your views here.
 
-# def blogview(request):
-#     return render(request, 'blog/blog.html')
-
 def blog(request):
     post = Blog.objects.all()
     context = {'post': post}
