9314fc58f7cc2d82b0ebfde3510f9d4715e48366
joseorozco84 <joseorozco84@gmail.com>
2022-06-20 15:12:14 -0300

    blog minor update

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,10 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = models.TextField()
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(blank=True, null=True)
     comments = models.IntegerField(default=0)
     category = models.CharField(max_length=255)
+    author = models.OneToOneField('auth.User', on_delete=models.CASCADE)
 
 
     def __str__(self):
