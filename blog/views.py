44e7d6eb70de97fed4beabfc8adf4402a05406e5
joseorozco84 <joseorozco84@gmail.com>
2022-06-20 12:44:04 -0300

    blog updates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from blog.models import Blog
 
 # Create your views here.
+
+# def blogview(request):
+#     return render(request, 'blog/blog.html')
+
+def blog(request):
+    post = Blog.objects.all()
+    context = {'post': post}
+    return render(request, 'blog/blog.html', context=context)
