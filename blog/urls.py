e835add5654f558301d6d023ce9e100c63d8101a
joseorozco84 <joseorozco84@gmail.com>
2022-06-20 21:04:24 -0300

    blog mayor updates

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from blog.views import blog, post_form
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', blog, name='blog'),
     path('post_form/', post_form, name='post_form'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Blog
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 
@@ -11,17 +13,27 @@ def blog(request):
     return render(request, 'blog/blog.html', context=context)
 
 # formulario para crear posts
+@login_required(login_url='login')
 def post_form(request):
-    data = {
-        'form' : PostForm(),
-    }
     if request.method == 'POST':
-        form = PostForm(data=request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return render(request, 'blog/post_form.html') 
+            formx = form.save(commit=False)
+            formx.save()
+            return redirect('blog')
         else:
-            data['form'] = form
-    return render(request, 'blog/post_form.html', context=data)
+            messages.error(request, 'Bumbula')
+            return render(request, 'blog/post_form.html')
+    return render(request, 'blog/post_form.html')
 
-
+# @login_required(login_url='login')
+# def post_form(request):
+#     if request.method == 'POST':
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.error(request, 'Bumbula')
+#             return render(request, 'blog/blog.html') 
+#         else:
+#             return render(request, 'blog/post_form.html')
+#     return render(request, 'blog/post_form.html')
