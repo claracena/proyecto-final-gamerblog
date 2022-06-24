e7507b5f31dc6493779fb7b3a8f029a1426aea58
claracena <claracena@users.noreply.github.com>
2022-06-23 22:06:01 -0300

    fixes

--- contact/urls.py
+++ contact/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.contact, name='contact'),
-    path('', views.success, name='success'),
+    path('success/', views.success, name='success'),
 ]
--- gamerblog/settings.py
+++ gamerblog/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'staticapp.apps.StaticappConfig',
     'account.apps.AccountConfig',
-    'contact'
+    'contact',
     'blog.apps.BlogConfig',
     'ckeditor',
     'ckeditor_uploader',
