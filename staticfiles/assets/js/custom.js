02ef17160486eb73e55d4ef25a697cae4861a128
claracena <claracena@users.noreply.github.com>
2022-06-26 18:42:45 -0300

    collect

---
+++ staticfiles/assets/js/custom.js
@@ -0,0 +1,64 @@
+$( document ).ready(function() {
+    // const swiper = new Swiper('.swiper', {
+    //     // Optional parameters
+    //     direction: 'horizontal',
+    //     loop: true,
+
+    //     // If we need pagination
+    //     pagination: {
+    //     el: '.swiper-pagination',
+    //     },
+
+    //     // Navigation arrows
+    //     navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    //     },
+
+    //     // And if we need scrollbar
+    //     // scrollbar: {
+    //     //   el: '.swiper-scrollbar',
+    //     // },
+    // });
+
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+    // var quill = new Quill('#editor', {
+    //     theme: 'snow'
+    // });
+
+});
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        navbar.classList.add("sticky") // para que se vea el navbar cuando se desplaza el scroll
+        $(".custom-top-menu").css('background-color', '#000000cc');
+        $(".custom-top-menu").css('padding', '10px'); // para que se oscurezca el navbar cuando se desplaza el scroll
+        mybutton.style.display = "block"; // para que se vea el boton cuando se desplaza el scroll
+    } else {
+        navbar.classList.remove("sticky");
+        $(".custom-top-menu").css('background-color', '#00000099');
+        $(".custom-top-menu").css('padding', '27px');
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function Copy() {
+    let url = document.location.href
+
+    navigator.clipboard.writeText(url)
+}
---
+++ staticfiles/ckeditor/ckeditor/config.js
@@ -0,0 +1,13 @@
+/**
+ * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	config.uiColor = '#F8991D';
+	// config.resize_minWidth = 500;
+	config.width = 'auto';
+	config.editorplaceholder = 'Escriba el contenido de su artículo aquí...';
+};
