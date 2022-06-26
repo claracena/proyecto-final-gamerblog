4a73f524f260622095fda5c4b7815e1ab018f50c
claracena <claracena@users.noreply.github.com>
2022-06-26 19:20:50 -0300

    white

--- gamerblog/settings.py
+++ gamerblog/settings.py
@@ -163,7 +163,7 @@ STATICFILES_DIRS = [
     # BASE_DIR / "static",
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'assets', 'media')
 
