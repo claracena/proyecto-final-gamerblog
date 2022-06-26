f015df3ddb0b6a922ba1ebc13ced6b714b3815d6
claracena <claracena@users.noreply.github.com>
2022-06-26 19:23:12 -0300

    mas whites

--- gamerblog/settings.py
+++ gamerblog/settings.py
@@ -163,7 +163,7 @@ STATICFILES_DIRS = [
     # BASE_DIR / "static",
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'assets', 'media')
 
