b0ec8b9c5b3e77d5e4d552163bdbcc64d310b1e3
claracena <claracena@users.noreply.github.com>
2022-06-27 00:46:29 -0300

    dbaws

--- gamerblog/settings.py
+++ gamerblog/settings.py
@@ -97,28 +97,27 @@ WSGI_APPLICATION = 'gamerblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         # 'OPTIONS': {
-#         #     'service': 'gamerblog',
-#         #     'passfile': '.pgpass',
-#         # },
-#         'NAME': 'gamerblog',
-#         'USER': 'gamer',
-#         'PASSWORD': 'Genius2008',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'OPTIONS': {
+        #     'service': 'gamerblog',
+        #     'passfile': '.pgpass',
+        # },
+        'NAME': 'gamerblogdb',
+        'USER': 'postgres',
+        'PASSWORD': 'GeniuS2010',
+        'HOST': 'gamerblogrds.civsttv3fcm1.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+#     }
+# }
 
 import dj_database_url 
 db_from_env = dj_database_url.config(conn_max_age=600)
