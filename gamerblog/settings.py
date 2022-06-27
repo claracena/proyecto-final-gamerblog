877196979b762e41f1e2bfd9f4b6f05d7efa573e
claracena <claracena@users.noreply.github.com>
2022-06-27 01:10:17 -0300

    meria raiz

--- gamerblog/settings.py
+++ gamerblog/settings.py
@@ -171,7 +171,7 @@ if USE_S3:
     AWS_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/assets/media/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     CKEDITOR_BASEPATH = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/ckeditor/ckeditor/'
 else:
